a0a42d925bb730dc48568552930e890a038260dc
veilia06 <veilia06@rg16.at>
2023-02-23 14:57:35 +0100

    nimm 2

--- main.py
+++ main.py
@@ -1,45 +1,23 @@
-# Caesar - Verschlüsselung
-
-clear_msg = input("Welche Nachricht soll verschlüsselt werden?")
-shift = int(input("Welchen Schlüssel möchtest du verwenden?"))
-# beschränke die Verschiebung auf max 26
-shift = shift % 26
-
 cypher_msg = ""
-clear_msg = clear_msg.upper()
-for char in clear_msg:
-    nb = ord(char)
-    if char.isalpha():
-        new_nb = nb + shift
-        if new_nb > 90:
-            new_nb -= 26
-    else:
-        new_nb = nb
-    new_char = chr(new_nb)
-    #print(char, nb, new_nb, new_char)
-    cypher_msg += new_char
-
-print(f"Die veschlüsselte Nachricht lautet: + {cypher_msg}")
 
 
 
-def encrypt(clear_msg, shift):
-    # beschränke die Verschiebung auf max 26
-    shift = shift % 26
-
-    cypher_msg = ""
-    clear_msg = clear_msg.upper()
-    for char in clear_msg:
-        nb = ord(char)
-        if char.isalpha():
-            new_nb = nb + shift
-            if new_nb > 90:
-                new_nb -= 26
-        else:
-            new_nb = nb
-        new_char = chr(new_nb)
-        #print(char, nb, new_nb, new_char)
-        cypher_msg += new_char
-    return cypher_msg
 
 
+
+
+nb = 19
+while nb > 0:
+    if nb > 7:
+        print("/")
+        nb -= 8
+    elif nb > 3:
+        print("|")
+        nb -= 4
+    elif nb > 1:
+        print(":")
+        nb -= 2
+    elif nb > 0:
+        print("°")
+        nb -= 1
+print(".")
