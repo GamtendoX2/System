3015569a84d12e56ed12909810b95b8139184ca6
veilia06 <veilia06@rg16.at>
2023-02-23 15:25:02 +0100

    provisorisch fertig

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
+clear_msg = input("Welche Nachricht soll verschlüsselt werden?")
+
+
+#clear_msg = ""
+clear_msg = clear_msg.lower()
 cypher_msg = ""
+for char in clear_msg:
+    if char == " ":
+        cypher_msg += "-"
+    elif char == ".":
+        cypher_msg += "_"
+    elif char == "?":
+        cypher_msg += "Y"
+    elif char == "!":
+        cypher_msg += "X"
 
-
-
-
-
-
-
-nb = 19
-while nb > 0:
-    if nb > 7:
-        print("/")
-        nb -= 8
-    elif nb > 3:
-        print("|")
-        nb -= 4
-    elif nb > 1:
-        print(":")
-        nb -= 2
-    elif nb > 0:
-        print("°")
-        nb -= 1
-print(".")
+    else:
+        nb = (ord(char) -96)
+        while nb > 0:
+            if nb > 7:
+                cypher_msg += "/"
+                nb -= 8
+            elif nb > 3:
+                cypher_msg += "|"
+                nb -= 4
+            elif nb > 1:
+                cypher_msg += ":"
+                nb -= 2
+            elif nb > 0:
+                cypher_msg += "°"
+                nb -= 1
+        cypher_msg += "."
+print(cypher_msg)
