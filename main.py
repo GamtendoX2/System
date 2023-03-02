10585691f237cec1a47022b63cc3b6c6eeed4e40
stechr06 <stechr06@rg16.at>
2023-03-02 15:27:41 +0100

    unittest

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 clear_msg = input("Welche Nachricht soll verschlüsselt werden?")
 
-
-#clear_msg = ""
+# clear_msg = ""
 clear_msg = clear_msg.lower()
 cypher_msg = ""
+wasNumber = False
 for char in clear_msg:
     if char == " ":
         cypher_msg += "-"
@@ -13,9 +13,11 @@ for char in clear_msg:
         cypher_msg += "Y"
     elif char == "!":
         cypher_msg += "X"
-
-    else:
-        nb = (ord(char) -96)
+    elif char.isdecimal():
+        nb = int(char)
+        if wasNumber == False:
+            cypher_msg += "Z"
+        wasNumber = True
         while nb > 0:
             if nb > 7:
                 cypher_msg += "/"
@@ -30,4 +32,26 @@ for char in clear_msg:
                 cypher_msg += "°"
                 nb -= 1
         cypher_msg += "."
+
+    else:
+        if wasNumber == True:
+            cypher_msg += "Z"
+        wasNumber = False
+        nb = (ord(char) - 96)
+        while nb > 0:
+            if nb > 7:
+                cypher_msg += "/"
+                nb -= 8
+            elif nb > 3:
+                cypher_msg += "|"
+                nb -= 4
+            elif nb > 1:
+                cypher_msg += ":"
+                nb -= 2
+            elif nb > 0:
+                cypher_msg += "°"
+                nb -= 1
+        cypher_msg += "."
+if wasNumber == True:
+    cypher_msg += "Z"
 print(cypher_msg)
