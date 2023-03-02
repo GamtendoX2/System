1f9d9a6ed78ad8403f44358bb500a566d5744302
veilia06 <veilia06@rg16.at>
2023-03-02 14:59:11 +0100

    Zahlen funktionieren

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ clear_msg = input("Welche Nachricht soll verschlüsselt werden?")
 #clear_msg = ""
 clear_msg = clear_msg.lower()
 cypher_msg = ""
+wasNumber = False
 for char in clear_msg:
     if char == " ":
         cypher_msg += "-"
@@ -13,8 +14,30 @@ for char in clear_msg:
         cypher_msg += "Y"
     elif char == "!":
         cypher_msg += "X"
+    elif char.isdecimal():
+        nb = int(char)
+        if wasNumber == False:
+            cypher_msg += "Z"
+        wasNumber = True
+        while nb > 0:
+            if nb > 7:
+                cypher_msg += "/"
+                nb -= 8
+            elif nb > 3:
+                cypher_msg += "|"
+                nb -= 4
+            elif nb > 1:
+                cypher_msg += ":"
+                nb -= 2
+            elif nb > 0:
+                cypher_msg += "°"
+                nb -= 1
+        cypher_msg += "."
 
     else:
+        if wasNumber == True:
+            cypher_msg += "Z"
+        wasNumber = False
         nb = (ord(char) -96)
         while nb > 0:
             if nb > 7:
@@ -30,4 +53,6 @@ for char in clear_msg:
                 cypher_msg += "°"
                 nb -= 1
         cypher_msg += "."
+if wasNumber == True:
+    cypher_msg += "Z"
 print(cypher_msg)
