8ee9578ab3388fb693e7ef755764cec8c4673891
stechr06 <stechr06@rg16.at>
2023-03-09 15:06:27 +0100

    test cases christian

--- unittest.py
+++ unittest.py
@@ -1,7 +1,7 @@
 import unittest
 
-
 def encrypt(clear_msg):
+    #clear_msg = ""
     clear_msg = clear_msg.lower()
     cypher_msg = ""
     wasNumber = False
@@ -38,7 +38,7 @@ def encrypt(clear_msg):
             if wasNumber == True:
                 cypher_msg += "Z"
             wasNumber = False
-            nb = (ord(char) - 96)
+            nb = (ord(char) -96)
             while nb > 0:
                 if nb > 7:
                     cypher_msg += "/"
@@ -61,15 +61,15 @@ def encrypt(clear_msg):
 class EncryptTest(unittest.TestCase):
     def test_encrypt_lower_single_letter_success(self):
         assert encrypt(clear_msg="a") == "°."
-        assert encrypt(clear_msg="b") == ":."
+        assert  encrypt(clear_msg="b") == ":."
 
     def test_encrypt_upper_single_letter_success(self):
         assert encrypt(clear_msg="A") == "°."
-        assert encrypt(clear_msg="B") == ":."
+        assert  encrypt(clear_msg="B") == ":."
 
     def test_encrypt_numbers_success(self):
         assert encrypt(clear_msg="1") == "Z°.Z"
-        assert encrypt(clear_msg="12") == "Z°.:.Z"
+        assert  encrypt(clear_msg="12") == "Z°.:.Z"
 
-    #def test_encrypt_all_success(self):
-        #assert encrypt(clear_msg=)
+    def test_encrypt_all_success(self):
+        assert encrypt(clear_msg="A1b2.") == "°.Z°.Z:.Z:.Z_"
